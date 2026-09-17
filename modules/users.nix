f8013a1c4ec3188330f3c54546fb1538c9959be8
Ivan <ivan@hu-tao.dev>
2026-09-17 11:41:08 +0300

    feat(ssh): add laptop key

--- modules/users.nix
+++ modules/users.nix
@@ -17,6 +17,7 @@
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID49dv6XQCkieSTgT8fPD54NScv30jNDI7Z0QhEbz57v hutao@hutao"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIoqi3O0lsZ/4eZfwt39yUxInELGG91ucSaF4d+fUKhU hutao@hutao-desktop"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAfw4ZR3O194CT9VNVMVv29DK1gaKCwxQp0CQRJwaSQ ivan@work-laptop"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJLLFqxc1Ihd4j676fikI7LH7WjXlEFkEr2g+d3090Rg hutao@hutao-laptop"
         ];
       };
       hutao = {
@@ -30,6 +31,7 @@
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID49dv6XQCkieSTgT8fPD54NScv30jNDI7Z0QhEbz57v hutao@hutao"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIoqi3O0lsZ/4eZfwt39yUxInELGG91ucSaF4d+fUKhU hutao@hutao-desktop"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAfw4ZR3O194CT9VNVMVv29DK1gaKCwxQp0CQRJwaSQ ivan@work-laptop"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJLLFqxc1Ihd4j676fikI7LH7WjXlEFkEr2g+d3090Rg hutao@hutao-laptop"
         ];
       };
     };
